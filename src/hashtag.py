c22033e54d1a2cecd9c530e06476a9df50c0f4c5
unknown <nsteinert@cocus.com>
2020-06-23 15:22:45 +0200

    update method added to Hashtag class

--- src/hashtag.py
+++ src/hashtag.py
@@ -1,16 +1,20 @@
+import csv
+import json
 import tweepy
 import src.locations as locations
 
 
 class Hashtag(tweepy.API):
 
-    def __init__(self, credentials):
+    def __init__(self, credentials, save_path='output\\search_results.csv'):
+        self.save_path = save_path
         super().__init__(credentials)
     
     def _check_validility(self):
         return self.verify_credentials()
     
     def set_location(self, location):
+        self.location = location
         self.coordinates = locations.longitude_latitude_dict[location]
         self.woeid = locations.woeid_dict[location]
     
@@ -22,11 +26,47 @@ class Hashtag(tweepy.API):
         for hashtag in self.hashtags:
             print(hashtag)
     
-    def count_hashtag_tweed(self):
-        for hashtag in self.hashtags:
-            pass
+    def get_hashtag_tweeds(self, hashtag):
+        hashtag_tweets = []
+        search_results = self.search(
+            q=hashtag,
+            geocode=self.coordinates,
+            result_type='current'
+        )
+        for search_result in search_results:
+            search_result_dict = {
+                'id': search_result.id,
+                'hashtag' : hashtag,
+                'text' : search_result.text,
+                'created_at' : search_result.created_at,
+                'retweeted' : search_result.retweeted,
+                'retweet_count' : search_result.retweet_count,
+                'favorite_count' : search_result.favorite_count,
+                'location': self.location,
+                'user': search_result.user.name
+            }
+            hashtag_tweets.append(search_result_dict)
 
-    def save_hashtags_tweeds(self):
-        pass
+        return hashtag_tweets
+
+    def hashtag_tweets_json(self):
+
+        hashtag_tweets_json = []
+        for hashtag in self.hashtags:
+            hashtag_tweets_json += self.get_hashtag_tweeds(hashtag)
+        return hashtag_tweets_json
+    
+    def update(self):
+
+        hashtag_tweets = self.hashtag_tweets_json()
+        hashtag_tweets.sort(key=lambda x: x['id'])
+        fnames = ['id','hashtag','text','created_at','retweeted','retweet_count','favorite_count','location','user']
+
+        with open(self.save_path, mode='a', encoding='utf-8') as output_csv:
+            writer = csv.DictWriter(output_csv, fieldnames=fnames)
+            for hashtag_tweet in hashtag_tweets:
+                writer.writerow(hashtag_tweet)
+        
+
     
 
